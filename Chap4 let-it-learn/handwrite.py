ef186cbfca27f9ebe67fea25390a4190e0345923
Bobbbbbbbbbby <578094246@outlook.com>
2023-05-23 16:38:55 +0800

    change output format

--- Chap4 let-it-learn/handwrite.py
+++ Chap4 let-it-learn/handwrite.py
@@ -12,7 +12,7 @@ train_loss_list = []
 iters_num = 10000
 train_size = len(x_train)
 batch_size = 100
-learning_rate = 0.01
+learning_rate = 0.1
 
 network = TwoLayerNet(784, 50, 10)
 
@@ -27,5 +27,5 @@ for i in range(iters_num) :
         network.params[key] -= learning_rate * grad[key]
 
     loss = network.loss(x_batch, t_batch)
-    print(loss, end='\t')
+    print(f"it: {i}\t, {i / iters_num}\t -- loss: {loss}")
     train_loss_list.append(loss)
